9682de7437a50e31bddc11a22171e5820c60ce11
Lars Danau <larsdanau@outlook.com>
2026-04-01 22:04:59 +0200

    fix: format files

--- apps/native/src/test/test-utils.tsx
+++ apps/native/src/test/test-utils.tsx
@@ -1,9 +1,4 @@
-import {
-  render,
-  renderHook,
-  screen,
-  userEvent,
-} from "@testing-library/react-native";
+import { render, renderHook, screen, userEvent } from "@testing-library/react-native";
 import type { ComponentType, PropsWithChildren, ReactElement } from "react";
 import { I18nextProvider } from "react-i18next";
 import i18n from "@/src/lib/i18n/i18n";
